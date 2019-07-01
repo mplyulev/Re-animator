80009ef481bcdc7ca9022ed753a5b185532056a6
Mihail Lyulev <mihail.lyulev@mihail.lyulev-mbp>
2019-07-01 12:04:59 +0300

    using request animationn frame now

--- src/Animated.js
+++ src/Animated.js
@@ -12,61 +12,62 @@ class Animated extends Component {
   }
 
   unmount = () => {
-    console.log('unmounting');
     this.setState({ isMounted: false })
   };
 
+
+
   render() {
-
-    // const style = {
-    //   backgroundColor: 'red',
-    // };
-
     const animations = {
-      height: {
+      left: {
         from: 0,
-        to: 100,
-        type: 'ease-in',
-      },
-      opacity: {
-        to: 1,
-        type: 'ease-in-out',
-      },
-      // paddingLeft: {
-      //   from: 1,
-      //   to: 1000,
-      //   speed: 10000,
-      //   type: 'ease-in'
-      // },
-      backgroundColor: {
-        from: 'gray',
-        to: 'white',
-        type: 'ease-in'
-      },
-      border: {
-        from: '1px solid white',
-        to: '5px dashed green',
-        type: 'linear',
-        speed: 5000
-      },
-      fontSize: {
-        from: 200,
-        to: 12,
-        type: 'ease-in'
-      },
-      color: {
-        from: 'red',
-        to: 'blue',
+        to: 300,
         type: 'linear'
       }
+      // height: {
+      //   from: 0,
+      //   to: 100,
+      //   type: 'ease-in',
+      // },
+      // opacity: {
+      //   to: 1,
+      //   type: 'ease-in-out',
+      // },
+      // // paddingLeft: {
+      // //   from: 1,
+      // //   to: 1000,
+      // //   speed: 10000,
+      // //   type: 'ease-in'
+      // // },
+      // backgroundColor: {
+      //   from: 'gray',
+      //   to: 'white',
+      //   type: 'ease-in'
+      // },
+      // border: {
+      //   from: '1px solid white',
+      //   to: '5px dashed green',
+      //   type: 'linear',
+      //   speed: 5000
+      // },
+      // fontSize: {
+      //   from: 200,
+      //   to: 12,
+      //   type: 'ease-in'
+      // },
+      // color: {
+      //   from: 'red',
+      //   to: 'blue',
+      //   type: 'linear'
+      // }
     }
 
     const arrays = ['asd', 'asad', 'asdasdasd', 'asdasdasdasd', 'asdasdasdasd', 'sadasdasdasd']
 
     return (
-      <Reanimate animations={animations} isMounted={this.state.isMounted} globalSpeed={10000} interval={1}>
-        {arrays.map(text => {
-          return <div onClick={this.unmount}>{text}</div>
+      <Reanimate animations={animations} isMounted={this.state.isMounted} globalSpeed={5000} interval={111}>
+        {arrays.map((text, index) => {
+          return <div className={`animated animated-${index}`} key={index} onClick={this.unmount}>{text}</div>
         })}
       </Reanimate>
 
--- src/Reanimate.js
+++ src/Reanimate.js
@@ -6,22 +6,25 @@ class Reanimate extends Component {
     super(props);
 
     this.state = {
-      style: null,
+      styles: [],
       children: [],
       childrenStyles: [],
       unmountChildren: false
     }
   }
 
-  animate = (isMounting) => {
-    let { animations, globalSpeed, interval } = this.props;
-    const style = {};
-    style.transition = ''
+  setStylesProps = (animations, currentStyle) => {
+    let style = {
+      transition: ''
+    };
+
+    const { globalSpeed } = this.props;
+
     let lowestSpeed = 0;
 
     Object.entries(animations).map(([key, value], index) => {
       if (typeof value.from !== 'undefined') {
-        style[key] = isMounting ? value.from : value.to;
+        style[key] = !currentStyle ? value.from : currentStyle[key];
       }
       let cssName = ''
       if (key !== key.toLowerCase()) {
@@ -31,7 +34,6 @@ class Reanimate extends Component {
           if (index !== keys.length - 1) {
             cssName += '-';
           }
-
         });
       }
 
@@ -41,44 +43,99 @@ class Reanimate extends Component {
       style.transition += `${cssName || key} ${(value.speed || globalSpeed) / 1000}s ${value.type}${index !== Object.entries(animations).length - 1 ? ', ' : ''}`;
     });
 
-    this.setState({ style });
+    return style;
+  }
 
+  animate = (isMounting) => {
+    let { animations, globalSpeed, interval } = this.props;
+    let animatedElements = [];
+    let styles = [];
+    let lowestSpeed = 0;
+    let style;
+
+
+    if (!isMounting) {
+      animatedElements = document.getElementsByClassName('animated');
+
+      Array.from(animatedElements).forEach((element) => {
+        const currentStyle = window.getComputedStyle(element);
+
+        styles.push(this.setStylesProps(animations, currentStyle));
+      });
+    }
+
+    style = this.setStylesProps(animations);
+    this.setState({ styles });
     const newStyle = Object.assign({}, style);
     Object.entries(animations).map(([key, value]) => {
       let endValue = isMounting ? value.to : value.from;
       newStyle[key] = endValue;
     });
-
-
     let children = [];
     let childrenStyles = [];
 
     if (lowestSpeed < globalSpeed) {
       lowestSpeed = globalSpeed;
     }
-
+    console.log('speed', lowestSpeed);
     if (!isMounting) {
-      setTimeout(() => {
+      this.requestTimeout(() => {
         this.setState({ unmountChildren: true });
-      }, lowestSpeed + 50);
+      }, lowestSpeed);
     }
 
 
+
+
+
     this.props.children.map((child, index) => {
-      console.log(child);
-      setTimeout(() => {
-        childrenStyles.push(style)
+      this.requestTimeout(() => {
+        childrenStyles.push(style);
         children.push(child);
-        this.setState({ children, childrenStyles });
-        setTimeout(() => {
+        this.setState({ children, childrenStyles: isMounting ? childrenStyles : styles });
+        console.log(styles);
+        this.requestTimeout(() => {
           childrenStyles[index] = newStyle;
           this.setState({ childrenStyles });
-        }, 50)
+        }, 0)
       }, interval * index);
     });
   }
 
+  requestTimeout = (fn, delay) => {
+    if (!window.requestAnimationFrame &&
+      !window.webkitRequestAnimationFrame &&
+      !(window.mozRequestAnimationFrame && window.mozCancelRequestAnimationFrame) && // Firefox 5 ships without cancel support
+      !window.oRequestAnimationFrame &&
+      !window.msRequestAnimationFrame)
+      return window.setTimeout(fn, delay);
+
+    var start = new Date().getTime(),
+      handle = new Object();
+
+    function loop() {
+      var current = new Date().getTime(),
+        delta = current - start;
+
+      delta >= delay ? fn.call() : handle.value = window.requestAnimFrame(loop);
+    };
+
+    handle.value = window.requestAnimFrame(loop);
+    return handle;
+  };
+
+
   componentDidMount() {
+    window.requestAnimFrame = (function () {
+      return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function (/* function */ callback, /* DOMElement */ element) {
+          window.setTimeout(callback, 1000 / 60);
+        };
+    })();
     this.animate(true)
   };
 
