41bfd47320f4b5f002f8b3095f4032c788a099e7
Mihail Lyulev <mihail.lyulev@mihail.lyulev-mbp>
2019-07-14 18:57:35 +0300

    ready

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import Animated from './Animated'
+import Reanimate from './Reanimate'
 class App extends Component {
-  render() {
-     
-    return (
-      <div className="app">
-         <Animated />
-      </div>
-    );
-  }
+    render() {
+
+        return (
+            <div className="app">
+                <Reanimate />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Reanimate.js
+++ src/Reanimate.js
@@ -1,252 +1,157 @@
 import React, { Component, Fragment } from 'react';
 import './Reanimate.css';
+const EQUATIONS_PER_DAY = 3
+const ADDITION = 'add';
+const SUBTRACTION = 'sub';
+const MULTIPLICATION = 'multiply';
+const DIVISION = 'div';
 
 class Reanimate extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            children: [],
-            style: {},
-            animatedChildrenKeys: [],
-            currentStyle: {},
-            elementsWithPendingAnimation: []
+            days: []
         }
     }
 
-    constructCSSPropName = (key) => {
-        let cssPropName = '';
-        const keys = key.match(/([A-Z]?[^A-Z]*)/g).slice(0, -1);
-        keys.forEach((keyPart, index) => {
-            cssPropName += keyPart.toLowerCase();
-            if (index !== keys.length - 1) {
-                cssPropName += '-';
-            }
-        });
+    generateEquation = (day, availableNumbers, operationType) => {
+        const x = Math.floor(Math.random() * availableNumbers) + 1;
+        const y = Math.floor(Math.random() * availableNumbers) + 1;
+        let finalResult;
 
-        return cssPropName;
-    }
-
-    setStylesProps = (animations, isUnmounting) => {
-        let style = {
-            transition: ''
-        };
-
-        let currentStyle = {
-            transition: ''
-        }
-        const { globalSpeed, exitAnimations } = this.props;
-        Object.entries(isUnmounting ? exitAnimations || animations : animations).map(([cssPropName, value], index) => {
-            if (isUnmounting) {
-                style[cssPropName] = exitAnimations !== undefined ? value.from : value.to;
-            }
-
-            if (!isUnmounting) {
-                style[cssPropName] = value.from;
-            }
-
-            currentStyle[cssPropName] = value.to;
-
-
-            if (cssPropName !== cssPropName.toLowerCase()) {
-                cssPropName = this.constructCSSPropName(cssPropName);
-            }
-
-            style.transition += `${cssPropName} ${(value.speed || globalSpeed) / 1000}s ${value.type}${index !== Object.entries(animations).length - 1 ? ', ' : ''}`;
-            currentStyle.transition = style.transition;
-        });
-
-        this.setState({ currentStyle });
-        return style;
-    }
-
-    animate = (isUnmounting, animatedChildrenKeys) => {
-        let { elementsWithPendingAnimation } = this.state;
-        const { exitAnimations, animations, globalSpeed, children } = this.props;
-        let style;
-
-        if (isUnmounting && exitAnimations !== undefined) {
-            style = this.setStylesProps(exitAnimations, false);
-        } else if (isUnmounting && exitAnimations === undefined) {
-            style = this.setStylesProps(animations, true);
-        } else if (!isUnmounting) {
-            style = this.setStylesProps(animations, false);
+        switch (operationType) {
+            case ADDITION:
+                finalResult = x + y
+                break;
+            case SUBTRACTION:
+                finalResult = x > y ? x - y : y - x;
+                break;
+            case DIVISION:
+                finalResult = x > y ? x / y : y / x;
+                break;
+            case MULTIPLICATION:
+                finalResult = x * y
+                break;
+            default:
         }
 
-        const newStyle = Object.assign({}, style);
-        let lowestSpeed = 0;
-        Object.entries(!isUnmounting ? animations : exitAnimations || animations).map(([key, value]) => {
-            newStyle[key] = value.to;
-            if (!isUnmounting || (isUnmounting && exitAnimations !== undefined)) {
-                newStyle[key] = value.to;
-            } else if (isUnmounting && exitAnimations === undefined) {
-                newStyle[key] = value.from;
-            }
-        });
+        let isInvalid = false;
 
-        if (lowestSpeed < globalSpeed) {
-            lowestSpeed = globalSpeed;
-        }
-
-        let previousChildren = this.state.children;
-
-        this.props.children.forEach(child => {
-            if (animatedChildrenKeys.includes(`.$${child.key}`)) {
-                previousChildren.push(child);
-            }
-        });
-
-        this.requestTimeout(() => {
-            this.setState({ style });
-            if (!isUnmounting && Object.values(elementsWithPendingAnimation).length === 0) {
-                this.setState({ children });
-            } else if (isUnmounting) {
-                animatedChildrenKeys.forEach(key => {
-                    elementsWithPendingAnimation.push({ key, newStyle });
-                });
-
-                let children = this.state.children;
-
-                this.state.children.forEach((child, index) => {
-                    if (animatedChildrenKeys.includes(`.$${child.key}`)) {
-                        const test = document.getElementById(child.key);
-                        test.addEventListener('transitionend', () => {
-                            test.style.display = 'none';
-                            elementsWithPendingAnimation = elementsWithPendingAnimation.filter((element) => !animatedChildrenKeys.includes(element.key));
-                            children.splice(index, 1);
-                            this.setState({ elementsWithPendingAnimation, children });
-                        });
-                    }
-                });
-            }
-
-            this.requestTimeout(() => {
-                this.setState({ style: newStyle });
-            }, 0)
-        }, 0);
-    }
-
-    requestTimeout = (fn, delay) => {
-        if (!window.requestAnimationFrame &&
-            !window.webkitRequestAnimationFrame &&
-            !(window.mozRequestAnimationFrame && window.mozCancelRequestAnimationFrame) && // Firefox 5 ships without cancel support
-            !window.oRequestAnimationFrame &&
-            !window.msRequestAnimationFrame)
-            return window.setTimeout(fn, delay);
-
-        const start = new Date().getTime(),
-            handle = new Object();
-
-        const loop = () => {
-            var current = new Date().getTime(),
-                delta = current - start;
-
-            delta >= delay ? fn.call() : handle.value = window.requestAnimFrame(loop);
-        };
-
-        handle.value = window.requestAnimFrame(loop);
-        return handle;
-    };
-
-    findRemovedChildrenKeys = (newChildren, oldChildren) => {
-        const animatedChildrenKeys = [];
-        const animatedChildren = [];
-
-        oldChildren.forEach(oldChild => {
-            if (!newChildren.find(newChild => oldChild.key === newChild.key)) {
-                animatedChildrenKeys.push(oldChild.key);
-                animatedChildren.push(oldChild);
-            }
-        });
-
-        return { animatedChildrenKeys, animatedChildren };
-    }
-
-    findAddedChildrenKeys = (newChildren, oldChildren) => {
-        const animatedChildrenKeys = [];
-        const animatedChildren = [];
-
-        newChildren.forEach(newChild => {
-            if (!oldChildren.find(oldChild => newChild.key === oldChild.key)) {
-                animatedChildrenKeys.push(newChild.key);
-                animatedChildren.push(newChild);
-            }
-        });
-
-        return { animatedChildrenKeys, animatedChildren };
-    }
-
-    componentWillReceiveProps(nextProps, nextState) {
-        const newChildren = React.Children.toArray(nextProps.children);
-        const oldChildren = React.Children.toArray(this.props.children);
-
-        if (newChildren.length !== oldChildren.length && newChildren.length < oldChildren.length) {
-            const animatedChildrenKeys = this.findRemovedChildrenKeys(newChildren, oldChildren).animatedChildrenKeys;
-            this.setState({ animatedChildrenKeys }, () => {
-                this.animate(true, animatedChildrenKeys);
+        isInvalid = this.state.days.some(day => {
+            day[operationType].some(equation => {
+                const { operand1, operand2 } = equation;
+                return (x === operand1 || x === operand2) && (y === operand1 || y === operand2);
             })
+        });
+
+        if (operationType === DIVISION && finalResult % 1 !== 0) {
+            return this.generateEquation(day, availableNumbers, operationType);
         }
 
-        if (newChildren.length !== oldChildren.length && newChildren.length > oldChildren.length) {
-            const animatedChildrenKeys = this.findAddedChildrenKeys(newChildren, oldChildren).animatedChildrenKeys;
-            this.setState({ animatedChildrenKeys }, () => {
-                this.animate(false, animatedChildrenKeys);
-            })
+        isInvalid = day[operationType].length > 0 && day[operationType].some(equation => {
+            const { operand1, operand2, result } = equation;
+            return operand1 === x || operand1 === y || operand2 === x || operand2 === y || result === finalResult
+        });
+
+        if (finalResult > availableNumbers || x === y) {
+            isInvalid = true;
         }
-    }
 
-    addRequestAnimFramePolyfill = () => {
-        window.requestAnimFrame = (() => {
-            return window.requestAnimationFrame ||
-                window.webkitRequestAnimationFrame ||
-                window.mozRequestAnimationFrame ||
-                window.oRequestAnimationFrame ||
-                window.msRequestAnimationFrame ||
-                function (/* function */ callback, /* DOMElement */ element) {
-                    window.setTimeout(callback, 1000 / 60);
-                };
-        })();
-    }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        if (nextState.children.length !== this.state.children.length || this.state.style !== nextState.style) {
-            return true;
+        if (operationType === SUBTRACTION) {
+            day[ADDITION].forEach(equation => {
+                const { operand1, operand2, result } = equation;
+                if (operand1 === x || operand1 === y || operand2 === x || operand2 === y) {
+                    isInvalid = true;
+                }
+            });
+        }
+
+        if (!isInvalid) {
+            return { operand1: x, operand2: y, result: finalResult }
         } else {
-            return false;
+            return this.generateEquation(day, availableNumbers, operationType);
         }
+    }
+
+    visualize = () => {
+
+    }
+
+    randomize = (days) => {
+        let availableNumbers = 40;
+        days.forEach((day, index) => {
+            availableNumbers += 2;
+            for (let i = 0; i < EQUATIONS_PER_DAY; i++) {
+                let addition = this.generateEquation(day, availableNumbers, ADDITION);
+                let subtraction = this.generateEquation(day, availableNumbers, SUBTRACTION);
+                let multiplication = this.generateEquation(day, availableNumbers, MULTIPLICATION);
+                let division = this.generateEquation(day, availableNumbers, DIVISION);
+
+                day[ADDITION].push(addition);
+                day[SUBTRACTION].push(subtraction);
+                day[MULTIPLICATION].push(multiplication);
+                day[DIVISION].push(division);
+            }
+        });
+
+        this.setState({ days })
     }
 
     componentDidMount() {
-        this.addRequestAnimFramePolyfill();
-        const children = React.Children.toArray(this.props.children);
-        const animatedChildrenKeys = this.findAddedChildrenKeys(children, []).animatedChildrenKeys;
-        this.setState({ animatedChildrenKeys }, () => {
-            animatedChildrenKeys && this.animate(false, animatedChildrenKeys);
+        const days = []
+
+        for (let i = 1; i <= 30; i++) {
+            const day = { number: i, add: [], sub: [], div: [], multiply: [] }
+            days.push(day);
+        }
+        this.setState({ days: days }, () => {
+            this.randomize(this.state.days);
         });
     };
 
     render() {
-        const { style, children, animatedChildrenKeys, currentStyle, elementsWithPendingAnimation } = this.state;
-        const childrenClone = React.Children.map(children, (child, index) => {
-            let childStyle = animatedChildrenKeys.includes(`.$${child.key}`) ? style : currentStyle;
-            const elementWithPendingAnimation = elementsWithPendingAnimation.find(element => element.key === `.$${child.key}`);
-            if (elementWithPendingAnimation) {
-                childStyle = elementWithPendingAnimation.newStyle;
-            }
-
-            const childClone = React.cloneElement(child, {
-                ...child.props,
-                id: child.key,
-                index,
-                style: childStyle
-            });
-
-            return childClone
-        });
+        const { days } = this.state;
 
         return (
             <Fragment>
-                {childrenClone}
+                {days.map(day => (
+                    <div className="day">
+                        <h5>DAY {day.number}</h5>
+                        <div className="equation">
+                            <h6>ADDITION</h6>
+                            {day.add.map(equation => (
+                                <p>{equation.operand1} + {equation.operand2} = {equation.result}</p>
+                            )
+                            )}
+                        </div>
+                        <div className="equation">
+                            <h6>SUBTRACTION</h6>
+                            {day.sub.map(equation => (
+                                <p>{equation.operand1 > equation.operand2 ? equation.operand1 : equation.operand2}
+                                    {` - `}   {equation.operand1 > equation.operand2 ? equation.operand2 : equation.operand1} = {equation.result}</p>
+                            )
+                            )}
+                        </div>
+                        <div className="equation">
+                            <h6>MULTIPLICATION</h6>
+                            {day.multiply.map(equation => (
+                                <p>{equation.operand1} * {equation.operand2} = {equation.result}</p>
+                            )
+                            )}
+                        </div>
+                        <div className="equation">
+                            <h6>DIVISION</h6>
+                            {day.div.map(equation => (
+                                <p>{equation.operand1 > equation.operand2 ? equation.operand1 : equation.operand2}
+                                    {` / `}   {equation.operand1 > equation.operand2 ? equation.operand2 : equation.operand1} = {equation.result}</p>
+                            )
+                            )}
+                        </div>
+                    </div>)
+
+                )}
             </Fragment >
         );
     }
